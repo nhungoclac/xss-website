0c994dfa87df7a5e404615d9e61b160649ab1521
dangkimanh01 <anhdangkim962@gmail.com>
2025-04-26 02:03:00 +0700

    hi
    đổi html và js. thêm mục ipv6

--- netlify-functions/logRequest.js
+++ netlify-functions/logRequest.js
@@ -1,16 +1,14 @@
 // netlify-functions/logRequest.js
 
 exports.handler = async (event, context) => {
-  const cookies = event.queryStringParameters.cookies || "No cookies";
   const userAgent = event.queryStringParameters.userAgent || "No user agent";
-  const userIP = event.queryStringParameters.userIP || "No IP";
-  const url = event.queryStringParameters.url || "No URL";
+  const userIP = event.queryStringParameters.userIP || "No IP (IPv4)";
+  const ipv6 = event.queryStringParameters.ipv6 || "No IPv6";
 
   // Ghi thông tin vào log
-  console.log(`Cookies: ${cookies}`);
   console.log(`User Agent: ${userAgent}`);
-  console.log(`IP: ${userIP}`);
-  console.log(`URL: ${url}`);
+  console.log(`IP (IPv4): ${userIP}`);
+  console.log(`IPv6: ${ipv6}`);
 
   return {
     statusCode: 200,
