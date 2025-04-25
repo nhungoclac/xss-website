5219d5c925ad5c57f2421899ae680ff9058dee84
dangkimanh01 <anhdangkim962@gmail.com>
2025-04-26 01:33:43 +0700

    26/4 1h33
    sửa file js

--- netlify-functions/logRequest.js
+++ netlify-functions/logRequest.js
@@ -1,7 +1,8 @@
 // netlify-functions/logRequest.js
 
 exports.handler = async (event, context) => {
-  const cookies = event.queryStringParameters.cookies || "No cookies";
+  // Lấy cookies từ headers
+  const cookies = event.headers.cookie || "No cookies";
   const url = event.queryStringParameters.url || "No URL";
 
   // Ghi thông tin vào log (có thể thay thế bằng việc lưu vào cơ sở dữ liệu hoặc gửi qua email)
