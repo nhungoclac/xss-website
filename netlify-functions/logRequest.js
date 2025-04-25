25fd209388e4a702efc78646e7028af08c2d8d25
dangkimanh01 <anhdangkim962@gmail.com>
2025-04-26 02:09:32 +0700

    sửa js và html

--- netlify-functions/logRequest.js
+++ netlify-functions/logRequest.js
@@ -2,13 +2,13 @@
 
 exports.handler = async (event, context) => {
   const userAgent = event.queryStringParameters.userAgent || "No user agent";
-  const userIP = event.queryStringParameters.userIP || "No IP (IPv4)";
-  const ipv6 = event.queryStringParameters.ipv6 || "No IPv6";
+  const userIP = event.queryStringParameters.userIP || "No IP";
+  const url = event.queryStringParameters.url || "No URL";
 
   // Ghi thông tin vào log
   console.log(`User Agent: ${userAgent}`);
-  console.log(`IP (IPv4): ${userIP}`);
-  console.log(`IPv6: ${ipv6}`);
+  console.log(`IP: ${userIP}`);
+  console.log(`URL: ${url}`);
 
   return {
     statusCode: 200,
