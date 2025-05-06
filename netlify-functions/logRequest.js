a6752564bd6cb37391356cebe767df5ffc02df77
dangkimanh01 <anhdangkim962@gmail.com>
2025-05-06 18:50:29 +0700

    bỏ cookie

--- netlify-functions/logRequest.js
+++ netlify-functions/logRequest.js
@@ -1,13 +1,13 @@
 // netlify-functions/logRequest.js
 
 exports.handler = async (event, context) => {
-  const cookies = event.queryStringParameters.cookies || "No cookies";
+  // const cookies = event.queryStringParameters.cookies || "No cookies";
   const userAgent = event.queryStringParameters.userAgent || "No user agent";
   const userIP = event.queryStringParameters.userIP || "No IP";
   const url = event.queryStringParameters.url || "No URL";
 
   // Ghi thông tin vào log
-  console.log(`Cookies: ${cookies}`);
+  // console.log(`Cookies: ${cookies}`);
   console.log(`User Agent: ${userAgent}`);
   console.log(`IP: ${userIP}`);
   console.log(`URL: ${url}`);
